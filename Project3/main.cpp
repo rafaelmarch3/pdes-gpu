f5271ee5f7ddf407922534effdadc632e1a441cb
rafaelmarch3 <rafaelmarch3@gmail.com>
2024-03-26 08:37:06 -0300

    A few changes

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -9,12 +9,12 @@ using namespace std;
 int main() {
 
 	// Main parameters
-	parameters::nx = 10;
-	parameters::ny = 10;
-	parameters::dx = 1;
-	parameters::dy = 1;
+	parameters::nx = 1000;
+	parameters::ny = 1000;
+	parameters::dx = 1. / parameters::nx;
+	parameters::dy = 1. / parameters::ny;
 	parameters::nt = 10;
-	parameters::dt = 30;
+	parameters::dt = 0.001;
 
 	// Dirichlet Boundary Values
 	parameters::uLeft = 2.5;
@@ -39,14 +39,6 @@ int main() {
 			D[j * parameters::nx + i] = Dconst;
 		}
 	}
-
-	/*
-	int nx = parameters::nx;
-	int ny = parameters::ny;
-	int n = nx * ny;
-	double* Ap = new double[n]; // Matrix-vector product
-	implicitDiffusionMatVecProduct(Ap, u, D);
-	*/
 	
 	// Time loop
 	double time = 0.0;
@@ -55,23 +47,12 @@ int main() {
 		cout << "Step " << n << endl;
 		
 		// Advancing field
-		//explicitDiffusion(u, uold, D);
-
-		// Advancing field
-		//implicitDiffusion(u, uold, D);
-
-		// Advancing field
 		implicitDiffusionCG(u, uold, D);
-		
-		// Printing Array
-		//print2DArray(u, parameters::nx, parameters::ny);
-		cout << endl << endl;
 
 		// Updating uold
 		copyArray(uold, u, parameters::nx * parameters::ny);
 
 	}
-	/**/
 
 	string outFolder = "D:/personal/studies/pdes-modern-cpp-and-cuda/pdes-gpu/out/";
 	string prefix = "field_final";
@@ -80,7 +61,6 @@ int main() {
 	writeScalarFieldToFile(u,
 		parameters::nx, parameters::ny,
 		fullpath_bin.c_str());
-	/**/
 
 	
 	return 0;
